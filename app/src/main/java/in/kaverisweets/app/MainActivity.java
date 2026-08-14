b7dda7eb467817a8e0e03542e2fca50c9cfa4fd0
aatifrsiddique-jpg <aatifrsiddique@gmail.com>
2026-08-14 19:04:44 +0530

    Update MainActivity.java

--- app/src/main/java/in/kaverisweets/app/MainActivity.java
+++ app/src/main/java/in/kaverisweets/app/MainActivity.java
@@ -1,14 +1,24 @@
 package in.kaverisweets.app;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        TextView textView = new TextView(this);
+
+        textView.setText("Kaveri Sweets");
+        textView.setTextSize(28);
+        textView.setTextColor(Color.rgb(75, 22, 13));
+        textView.setGravity(Gravity.CENTER);
+
+        setContentView(textView);
     }
 }
